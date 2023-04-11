6ae2f534d727580416a28e9e0c63bb48e7e566ab
jpates1 <jamesandrewpates@gmail.com>
2023-04-11 09:26:17 +0100

    add test for createdDate

--- api/spec/models/post.spec.js
+++ api/spec/models/post.spec.js
@@ -11,9 +11,11 @@ describe("Post model", () => {
   });
 
   it("has a message with a created date", () => {
+  it("has a message with a created date", () => {
     var post = new Post({ message: "some message" });
     expect(post.message).toEqual("some message");
-    expect(post.dateCreated.getTime() / 1000).toBeCloseTo(Math.floor(Date.now() / 1000), -2);
+    expect(post.dateCreated).toBeCloseTo(Math.floor(Date.now() / 1000), -2);
+
   });
 
   it("can list all posts", (done) => {
