e85bc330e567428c1ed3a9e927a8abfb4ac5c067
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-04-12 23:46:24 +0100

    Added code for photo to CreatePostForm and axios

--- frontend/src/components/createpost/CreatePostForm.js
+++ frontend/src/components/createpost/CreatePostForm.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const CreatePostForm = ({ navigate }) => {
 
   const [message, setMessage] = useState("");
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [image, setImage] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // added another IF here because I am getting a 401 error (missing token)
+    if(token) {
+      const formData = new FormData();
+      formData.append('photo', image);
+
+      axios.post('http://localhost:3000/posts', formData)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
 
     if(token) {
       let response = await fetch( '/posts', {
@@ -35,9 +51,15 @@ const CreatePostForm = ({ navigate }) => {
     setMessage(event.target.value)
   }
 
+  const handlePhoto = (event) => {
+    setImage(event.target.files[0]);
+    console.log(image)
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} encType='multipart/form-data'>
       <input placeholder="Message" id="message" type='text' value={ message } onChange={handleMessageChange} />
+      <input id="photo" accept= ".png, .jpg, .jpeg" type='file' onChange={handlePhoto} />
       <input id='submit' type="submit" value="Submit" />
     </form>
   );
