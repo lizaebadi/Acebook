2e8435e13a5e2ec4af8cc98c604422101c494ef1
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-04-14 00:51:12 +0100

    photo url is now being added to the DB

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -12,19 +12,23 @@ const PostsController = {
     });
   },
   Create: (req, res) => {
+    let photoUrl = null;
+    if (req.file) {
+      photoUrl = `/uploaded_photos/${req.file.filename}`;
+    }
     const post = new Post({
       message: req.body.message,
-      photoUrl: req.file ? `/uploaded_photos/${req.file.filename}` : null
+      photoUrl: photoUrl,
     });
     post.save(async (err) => {
       if (err) {
         throw err;
       }
-  
       const token = await TokenGenerator.jsonwebtoken(req.user_id);
-      res.status(201).json({ message: 'OK', token: token });
+      res.status(201).json({ message: "OK", token: token });
     });
-  }  
+  },
+  
 };
 
 module.exports = PostsController;
