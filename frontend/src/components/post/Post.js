c0e2921e024b761c01ade32114c025433487934c
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-04-14 10:21:07 +0100

    Pictures are now displayed as posts

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const Post = ({post}) => {
-  return(
-    <article data-cy="post" key={ post._id }>{ post.message }</article>
-  )
-}
+const Post = ({ post }) => {
+  return (
+    <article data-cy="post" key={post._id}>
+      {post.message}
+      {post.photoUrl && <img src={post.photoUrl} alt="post" />}
+    </article>
+  );
+};
 
 export default Post;
+
+// import React from 'react';
+
+// const Post = ({ post }) => {
+//   return (
+//     <article data-cy="post" key={post._id}>
+//       {post.message && <p>{post.message}</p>}
+//       {post.photoUrl && <img src={post.photoUrl} alt="post" />}
+//     </article>
+//   );
+// };
+
+// export default Post;
+
